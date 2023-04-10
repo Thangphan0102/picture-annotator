a9aa24d5064347a7e36e63cde031f5942f8f804c
Thang Phan <thang.phancao@aalto.fi>
2023-04-10 23:03:04 +0300

    added image.py

---
+++ src/image.py
@@ -0,0 +1,18 @@
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+
+class Image(QPixmap):
+    def __init__(self, file_path):
+        super(Image, self).__init__(file_path)
+
+        self.labels = []
+        self.bounding_boxes = []
+        self.label_color_dict = {}
+
+    def add_label(self, label):
+        self.labels.append(label)
+
+    def add_bounding_box(self, start_point, end_point):
+        self.bounding_boxes.append([start_point.x(), start_point.y(), end_point.x(), end_point.y()])
