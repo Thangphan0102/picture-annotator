6034cce43d4b2a5d67dd582ae35c0b660c1e2073
Thang Phan <thang.phancao@aalto.fi>
2023-04-10 23:02:44 +0300

    rewrite canvas.py

--- src/canvas.py
+++ src/canvas.py
@@ -1,41 +1,60 @@
-import os
+import sys
 from pathlib import Path
-
 from pascal_voc_writer import Writer
-from PyQt6.QtCore import Qt, QPoint, QRect
-from PyQt6.QtGui import QImage, QPainter, QPen, QColor, QAction, QTextItem, QColorConstants, QMouseEvent, QPaintEvent, QEnterEvent
-from PyQt6.QtWidgets import QWidget, QInputDialog, QColorDialog
+
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+from image import *
 
 
 class Canvas(QWidget):
     def __init__(self, image_path, *args, **kwargs):
         super(Canvas, self).__init__(*args, **kwargs)
-        self.image_path = image_path
-        self.image = QImage(self.image_path)
-        self.setFixedSize(self.image.width(), self.image.height())
 
-        self.revisions = []
-        self.labels = []
-        self.bounding_boxes = []
-        self.label_color_dict = {}
+        self.image = Image(image_path)
 
-        self.pen = QPen()
-        self.pen.setColor(QColorConstants.Red)
-        self.pen.setWidth(3)
-
-        self.idle = True
         self.drawing = False
+        self.idle = True
         self.guide_line_on = True
-
-        self.position = QPoint()
-        self.start_point = None
-        self.end_point = None
-        self.rectangle_width = None
-        self.rectangle_height = None
+        self.mouse_pos = None
 
         self.create_shortcuts()
-
         self.setMouseTracking(True)
+        self.setFixedSize(self.image.width(), self.image.height())
+
+    def paintEvent(self, event: QPaintEvent):
+        p = QPainter(self)
+        rect = event.rect()
+        p.drawPixmap(rect, self.image, rect)
+
+        for label, bounding_box in zip(self.image.labels, self.image.bounding_boxes):
+            x1, y1, x2, y2 = bounding_box
+            bounding = QRect(x1, y1, x2 - x1, y2 - y1)
+            color = self.image.label_color_dict[label]
+            self.draw_rectangle(p, bounding, color, fill=False)
+            self.draw_text(p, label, x1, y1, x2, y2)
+
+        if self.guide_line_on and self.mouse_pos is not None:
+            p.setPen(QPen(QColorConstants.White, 1))
+
+            # Draw horizontal guide line
+            p.drawLine(0, self.mouse_pos.y(), self.image.width(), self.mouse_pos.y())
+
+            # Draw vertical guide line
+            p.drawLine(self.mouse_pos.x(), 0, self.mouse_pos.x(), self.image.height())
+
+        if self.drawing:
+            self.draw_rectangle(p)
+
+    def enterEvent(self, event: QEnterEvent) -> None:
+        if self.idle:
+            self.mouse_pos = event.position().toPoint()
+
+    def leaveEvent(self, event: QEvent) -> None:
+        self.mouse_pos = None
+        self.update()
 
     def mousePressEvent(self, event: QMouseEvent):
         if self.idle:
@@ -44,119 +63,52 @@ class Canvas(QWidget):
 
     def mouseMoveEvent(self, event: QMouseEvent):
         if self.idle:
-            if self.guide_line_on:
-                self.position = event.pos()
-                self.update()
+            self.mouse_pos = event.pos()
+            self.update()
 
             if event.buttons() & Qt.MouseButton.LeftButton:
                 self.drawing = True
                 self.end_point = event.pos()
                 self.check_mouse(event)
-                self.rectangle_width = self.end_point.x() - self.start_point.x()
-                self.rectangle_height = self.end_point.y() - self.start_point.y()
                 self.update()
 
     def mouseReleaseEvent(self, event: QMouseEvent):
-        if self.idle:
+        if self.idle and self.drawing:
             if event.button() == Qt.MouseButton.LeftButton:
-                self.add_label()
+                self.insert_label()
                 self.drawing = False
                 self.update()
 
-    def enterEvent(self, event) -> None:
-        if self.idle:
-            self.position = event.position().toPoint()
-
-    def leaveEvent(self, event) -> None:
-        self.position = None
-        self.update()
-
-    def paintEvent(self, event: QPaintEvent):
-        qp = QPainter(self)
-        rect = event.rect()
-        qp.drawImage(rect, self.image, rect)
-        if self.guide_line_on and self.position is not None:
-            qp.setPen(QPen(QColorConstants.White, 3))
-
-            # Draw horizontal guide line
-            qp.drawLine(0, self.position.y(), self.image.width(), self.position.y())
-
-            # Draw vertical guide line
-            qp.drawLine(self.position.x(), 0, self.position.x(), self.image.height())
-
-        if self.drawing:
-            self.draw_rectangle(qp)
-
-    def draw_rectangle(self, qp, color=QColorConstants.Red, fill=True):
-        qp.setRenderHint(QPainter.RenderHint.Antialiasing)
-        self.pen.setColor(color)
-        qp.setPen(self.pen)
-        rect = QRect(self.start_point.x(), self.start_point.y(), self.rectangle_width, self.rectangle_height)
-        qp.drawRect(rect)
+    def draw_rectangle(self, p, rect=None, color=QColorConstants.Red, fill=True):
+        p.setRenderHint(QPainter.RenderHint.Antialiasing)
+        p.setPen(QPen(color, 3))
+        if rect is None:
+            rect = QRect(
+                self.start_point.x(), self.start_point.y(),
+                self.end_point.x() - self.start_point.x(), self.end_point.y() - self.start_point.y()
+            )
+        p.drawRect(rect)
         if fill:
-            qp.fillRect(rect, QColor(255, 0, 0, 30))
+            p.fillRect(rect, QColor(255, 0, 0, 30))
 
-    def add_text(self, qp, text):
-        font = qp.font()
-        font.setPixelSize(14)
-        qp.setFont(font)
-        x_position = min(self.start_point.x(), self.end_point.x()) + 3
-        y_position = max(self.start_point.y(), self.end_point.y()) - 3
-        qp.drawText(QPoint(x_position, y_position), text)
-
-    def undo(self):
-        if self.revisions and self.labels:
-            self.image = self.revisions.pop()
-            label = self.labels.pop()
-            self.bounding_boxes.pop()
-            if label not in self.labels:
-                self.label_color_dict.pop(label)
-            self.update()
-
-    def reset(self):
-        if self.revisions and self.labels:
-            self.image = self.revisions[0]
-            self.revisions.clear()
-            self.labels.clear()
-            self.bounding_boxes.clear()
-            self.label_color_dict.clear()
-            self.update()
-
-    def save(self):
-        self.writer = Writer(self.image_path, self.image.width(), self.image.height())
-
-        for label, bounding_box in zip(self.labels, self.bounding_boxes):
-            xmin, ymin, xmax, ymax = bounding_box
-            self.writer.addObject(label, xmin, ymin, xmax, ymax)
-
-        data_path = Path(self.image_path).parent.parent
-        file_name = Path(self.image_path).name
-        export_file_name = Path(file_name).with_suffix('.xml')
-        export_path = Path(data_path).joinpath('annotations').joinpath(export_file_name)
-        self.writer.save(export_path)
-
-    def print_labels(self):
-        print(self.labels, self.bounding_boxes)
-        print(self.label_color_dict)
-
-    def add_label(self):
+    def insert_label(self):
         self.idle = False
-        qp = QPainter(self.image)
         label, ok = QInputDialog.getText(self, 'Class label', 'Enter class label')
         if ok:
-            if label not in self.label_color_dict.keys():
+            if label not in self.image.label_color_dict.keys():
                 color = QColorDialog.getColor(options=QColorDialog.ColorDialogOption.DontUseNativeDialog)
-                self.label_color_dict[label] = color
-            else:
-                color = self.label_color_dict[label]
-            self.revisions.append(self.image.copy())
-            self.draw_rectangle(qp, color=color, fill=False)
-            self.add_text(qp, label)
-            self.labels.append(label)
-            self.bounding_boxes.append(
-                [self.start_point.x(), self.start_point.y(), self.end_point.x(), self.end_point.y()]
-            )
+                self.image.label_color_dict[label] = color
+            self.image.add_label(label)
+            self.image.add_bounding_box(self.start_point, self.end_point)
         self.idle = True
+
+    def draw_text(self, p, text, x1, y1, x2, y2):
+        font = p.font()
+        font.setPixelSize(14)
+        p.setFont(font)
+        x_position = min(x1, x2) + 3
+        y_position = max(y1, y2) - 3
+        p.drawText(QPoint(x_position, y_position), text)
 
     def check_mouse(self, event):
         # Check if mouse move outside of image
@@ -166,22 +118,58 @@ class Canvas(QWidget):
         # ==|=======|==
         # 6 |   7   | 8
         image_x1, image_y1, image_x2, image_y2 = self.image.rect().getCoords()
-        if event.pos().x() < image_x1:                                      # 4
+        if event.pos().x() < image_x1:  # 4
             self.end_point = QPoint(image_x1 + 1, event.pos().y())
-        if event.pos().y() < image_y1:                                      # 2
+        if event.pos().y() < image_y1:  # 2
             self.end_point = QPoint(event.pos().x(), image_y1 + 1)
-        if (event.pos().x() < image_x1) and (event.pos().y() < image_y1):   # 1
+        if (event.pos().x() < image_x1) and (event.pos().y() < image_y1):  # 1
             self.end_point = QPoint(image_x1 + 1, image_y1 + 1)
-        if event.pos().x() > image_x2:                                      # 5
+        if event.pos().x() > image_x2:  # 5
             self.end_point = QPoint(image_x2, event.pos().y())
-        if event.pos().y() > image_y2:                                      # 7
+        if event.pos().y() > image_y2:  # 7
             self.end_point = QPoint(event.pos().x(), image_y2)
-        if (event.pos().x() > image_x2) and (event.pos().y() > image_y2):   # 8
+        if (event.pos().x() > image_x2) and (event.pos().y() > image_y2):  # 8
             self.end_point = QPoint(image_x2, image_y2)
-        if (event.pos().x() < image_x1) and (event.pos().y() > image_y2):   # 6
+        if (event.pos().x() < image_x1) and (event.pos().y() > image_y2):  # 6
             self.end_point = QPoint(image_x1 + 1, image_y2)
-        if (event.pos().x() > image_x2) and (event.pos().y() < image_y1):   # 3
+        if (event.pos().x() > image_x2) and (event.pos().y() < image_y1):  # 3
             self.end_point = QPoint(image_x2, image_y1 + 1)
+
+    ###########
+    # Actions and shortcuts
+    ###########
+    def undo(self):
+        if self.image.labels:
+            label = self.image.labels.pop()
+            self.image.bounding_boxes.pop()
+            if label not in self.image.labels:
+                self.image.label_color_dict.pop(label)
+            self.update()
+
+    def reset(self):
+        if self.image.labels:
+            self.image.labels.clear()
+            self.image.bounding_boxes.clear()
+            self.image.label_color_dict.clear()
+            self.update()
+
+    def save(self):
+        self.writer = Writer(self.image_path, self.image.width(), self.image.height())
+
+        for label, bounding_box in zip(self.image.labels, self.image.bounding_boxes):
+            x1, y1, x2, y2 = bounding_box
+            self.writer.addObject(label, x1, y1, x2, y2)
+
+        data_path = Path(self.image_path).parent.parent
+        file_name = Path(self.image_path).name
+        export_file_name = Path(file_name).with_suffix('.xml')
+        export_path = Path(data_path).joinpath('annotations').joinpath(export_file_name)
+        self.writer.save(export_path)
+
+    def print_labels(self):
+        print(self.image.labels)
+        print(self.image.bounding_boxes)
+        print(self.image.label_color_dict)
 
     def create_shortcuts(self):
         undo_action = QAction('Undo', self)
