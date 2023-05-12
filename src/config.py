dd6f9a0949024e033e5bd024fa5e6b47c3eb6d89
Thang Phan <thang.phancao@aalto.fi>
2023-05-12 11:30:12 +0300

    modified IMAGE_DIR

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 IMAGE_EXTENSIONS = ['*.jpeg', '*.jpg', '*.png']
 BASE_DIR = Path(__file__).parent.parent
 DATA_DIR = Path(BASE_DIR, 'data')
-IMAGE_DIR = None
+IMAGE_DIR = Path(DATA_DIR, 'images')
 ANNOTATION_DIR = Path(DATA_DIR, 'annotations')
 SRC_DIR = Path(BASE_DIR, 'src')
